6c80172fed402e9e5b8640ddd04ba88d07320c26
Le Vivilet <levivilet@pm.me>
2026-01-27 19:08:23 +0100

    fix

--- packages/opener-worker/src/parts/Listen/Listen.ts
+++ packages/opener-worker/src/parts/Listen/Listen.ts
@@ -1,12 +1,10 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import { initializeRendererProcess } from '../InitializeRendererProcess/InitializeRendererProcess.ts'
 
 export const listen = async (): Promise<void> => {
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
   RendererWorker.set(rpc)
-  await initializeRendererProcess()
 }
