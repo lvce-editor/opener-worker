ee6a242d5a6bcedb6ea9ea4f93804c3b75618c6e
Le Vivilet <levivilet@pm.me>
2026-01-27 19:19:45 +0100

    feature: implement memory opener for testing

--- packages/opener-worker/src/parts/OpenExternal/OpenExternal.ts
+++ packages/opener-worker/src/parts/OpenExternal/OpenExternal.ts
@@ -1,9 +1,18 @@
+import { get, writeUrl } from '../OpenerState/OpenerState.ts'
 import * as SharedProcess from '../SharedProcess/SharedProcess.ts'
 
-export const showItemInFolder = (fullPath: string): Promise<void> => {
+export const showItemInFolder = async (fullPath: string): Promise<void> => {
+  if (get()) {
+    writeUrl(fullPath)
+    return
+  }
   return SharedProcess.invoke('OpenExternal.showItemInFolder', fullPath)
 }
 
-export const openExternal = (url: string): Promise<void> => {
+export const openExternal = async (url: string): Promise<void> => {
+  if (get()) {
+    writeUrl(url)
+    return
+  }
   return SharedProcess.invoke('OpenExternal.openExternal', url)
 }
