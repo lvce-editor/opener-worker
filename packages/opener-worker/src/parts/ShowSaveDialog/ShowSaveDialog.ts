aa422075ed18521cee9e051c8a10c2d30894547b
Le Vivilet <levivilet@pm.me>
2026-02-01 22:07:03 +0100

    feature: update showSaveDialog function with additional TODO comments for web implementation (#21)

--- packages/opener-worker/src/parts/ShowSaveDialog/ShowSaveDialog.ts
+++ packages/opener-worker/src/parts/ShowSaveDialog/ShowSaveDialog.ts
@@ -1,5 +1,7 @@
 import { SharedProcess } from '@lvce-editor/rpc-registry'
 
 export const showSaveDialog = (title: string, properties: readonly string[]): Promise<any> => {
+  // TODO when running in web, maybe only make a prompt and ask for a filename
+  // and the use the workspace path as directory
   return SharedProcess.invoke('ElectronDialog.showSaveDialog', title, properties)
 }
