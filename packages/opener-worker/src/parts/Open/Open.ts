bbf3b6e0d9c777c23bc4f5ad8c893675ab846998
Le Vivilet <levivilet@pm.me>
2026-02-01 15:09:49 +0100

    feature: replace RendererProcess with RendererWorker in openUrlWeb function

--- packages/opener-worker/src/parts/Open/Open.ts
+++ packages/opener-worker/src/parts/Open/Open.ts
@@ -1,14 +1,15 @@
 import { PlatformType } from '@lvce-editor/constants'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { VError } from '@lvce-editor/verror'
 import * as ElectronWindow from '../ElectronWindow/ElectronWindow.ts'
 import { get, writeUrl } from '../OpenerState/OpenerState.ts'
 import * as OpenExternal from '../OpenExternal/OpenExternal.ts'
 import * as Platform from '../Platform/Platform.ts'
-import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 
 const openUrlWeb = async (url: string): Promise<void> => {
   try {
-    await RendererProcess.invoke(/* Open.openUrl */ 'Open.openUrl', /* url */ url)
+    // TODO make platform argument required
+    await RendererWorker.invoke('Open.openUrl', url)
   } catch (error) {
     throw new VError(error, `Failed to open url ${url}`)
   }
