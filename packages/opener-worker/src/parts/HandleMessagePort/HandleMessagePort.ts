5fa4b071c4e924d23c19b42a9b5ed11d48ebbce3
Le Vivilet <levivilet@pm.me>
2026-02-01 15:07:40 +0100

    fix

--- packages/opener-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
+++ packages/opener-worker/src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -1,7 +1,7 @@
 import { PlainMessagePortRpcParent } from '@lvce-editor/rpc'
 import { set } from '@lvce-editor/rpc-registry'
 
-export const handleMessagePort = async (port: MessagePort, rpcId?: number) => {
+export const handleMessagePort = async (port: MessagePort, rpcId?: number): Promise<void> => {
   const rpc = await PlainMessagePortRpcParent.create({
     commandMap: {},
     messagePort: port,
