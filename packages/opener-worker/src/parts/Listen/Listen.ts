c499fc689a105f21036e68b5e9332f15f98b7a79
Le Vivilet <levivilet@pm.me>
2026-02-01 15:33:29 +0100

    feature: ensure initializeMainProcess is called in listen function (#18)

--- packages/opener-worker/src/parts/Listen/Listen.ts
+++ packages/opener-worker/src/parts/Listen/Listen.ts
@@ -1,5 +1,7 @@
+import { initializeMainProcess } from '../InitializeMainProcess/InitializeMainProcess.ts'
 import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
   await initializeRendererWorker()
+  await initializeMainProcess()
 }
