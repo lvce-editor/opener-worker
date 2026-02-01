27dda4e2ca0595effc1f062fc6b1dfb100b48c86
Le Vivilet <levivilet@pm.me>
2026-02-01 22:15:37 +0100

    feature: add registerSaveDialogMock and clearSaveDialogMock functions to ShowSaveDialog (#22)

--- packages/opener-worker/src/parts/ShowSaveDialog/ShowSaveDialog.ts
+++ packages/opener-worker/src/parts/ShowSaveDialog/ShowSaveDialog.ts
@@ -1,7 +1,24 @@
-import { SharedProcess } from '@lvce-editor/rpc-registry'
+import { PlatformType } from '@lvce-editor/constants'
+import { RendererWorker, SharedProcess } from '@lvce-editor/rpc-registry'
 
-export const showSaveDialog = (title: string, properties: readonly string[]): Promise<any> => {
+let saveDialogMockReturnValue: any = null
+
+export const registerSaveDialogMock = (value: any): void => {
+  saveDialogMockReturnValue = value
+}
+
+export const clearSaveDialogMock = (): void => {
+  saveDialogMockReturnValue = null
+}
+
+export const showSaveDialog = async (title: string, properties: readonly string[], platform: number): Promise<any> => {
+  if (saveDialogMockReturnValue !== null) {
+    return saveDialogMockReturnValue
+  }
+  if (platform === PlatformType.Electron) {
+    return SharedProcess.invoke('ElectronDialog.showSaveDialog', title, properties)
+  }
   // TODO when running in web, maybe only make a prompt and ask for a filename
   // and the use the workspace path as directory
-  return SharedProcess.invoke('ElectronDialog.showSaveDialog', title, properties)
+  return RendererWorker.invoke('ConfirmPompt.prompt', title)
 }
